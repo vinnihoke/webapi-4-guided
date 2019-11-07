6e6d89d61deb8765dd38c644dc2ee95d2c6a2883
Vinni Hoke <vinni.hoke@gmail.com>
2019-11-06 20:23:11 -0800

    Added Kirkby's trycatch and MOTD

--- api/server.js
+++ api/server.js
@@ -1,33 +1,38 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const Shoutouts = require('../data/shoutouts-model.js');
+const Shoutouts = require("../data/shoutouts-model.js");
 
 const server = express();
+
+const { MOTD } = process.env;
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  Shoutouts.find()
-  .then(shoutouts => {
-    res.status(200).json(shoutouts);
-  })
-  .catch (error => {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  });
+server.get("/", async (req, res) => {
+  try {
+    const shouts = await db("shouts");
+    const motd = MOTD || "Hi World!!!";
+
+    res.status(200).json({ motd, shouts });
+  } catch (error) {
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot retrieve the shouts" });
+  }
 });
 
-server.post('/', (req, res) => {
-  Shoutouts.add(req.body)
-  .then(shoutout => {
-    res.status(201).json(shoutout);
-  })
-  .catch (error => {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shoutout' });
-  });
+server.post("/", async (req, res) => {
+  try {
+    const [id] = await db("shouts").insert(req.body);
+    console.log(req.body);
+    const shouts = await db("shouts");
+
+    res.status(201).json(shouts);
+  } catch (error) {
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot add the shout" });
+  }
 });
 
 module.exports = server;
